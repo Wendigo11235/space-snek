533989768a91aaf1027fd4118c8affabd0d80c43
Wendigo11235 <will.white.011235@gmail.com>
2026-05-11 10:49:58 -0500

    removed storage page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from 'react'
-import Nav from './components/Nav'
 import Game from './pages/Game'
-import Storage from './pages/Storage'
 
 function App() {
-   const [currentPage, setCurrentPage] = useState('game')
-   
-   return (
-       <div>
-           <Nav currentPage={currentPage} setCurrentPage={setCurrentPage} />
-           {currentPage === 'game' && <Game />}
-           {currentPage === 'storage' && <Storage />}
-       </div>
-   )
+  return (
+    <div>
+      <Game />
+    </div>
+  )
 }
 
 export default App
--- src/pages/Storage.jsx
+++
@@ -1,5 +0,0 @@
-function Storage() {
-    return <h2>Storage Page</h2>
-}
-
-export default Storage
